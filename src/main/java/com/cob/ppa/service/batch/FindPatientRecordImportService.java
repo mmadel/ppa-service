5ec331d52ff6e4e8f3e9e3722c87e315ca8f9e3c
madel <mohammed.adel.abo.foutouh@gmail.com>
2025-05-26 14:05:50 +0300

    change PatientRecordJobResponse to accept dto rather than entity

--- src/main/java/com/cob/ppa/service/batch/FindPatientRecordImportService.java
+++ src/main/java/com/cob/ppa/service/batch/FindPatientRecordImportService.java
@@ -1,25 +1,32 @@
 package com.cob.ppa.service.batch;
 
+import com.cob.ppa.dto.PatientRecordImportJobDTO;
 import com.cob.ppa.entity.PatientRecordImportJob;
 import com.cob.ppa.repository.PatientRecordJobRepository;
 import com.cob.ppa.response.patient.record.builder.PatientRecordBuilder;
 import com.cob.ppa.response.patient.record.model.PatientRecordJobResponse;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class FindPatientRecordImportService {
     @Autowired
     PatientRecordJobRepository patientRecordJobRepository;
-
+    @Autowired
+    ModelMapper mapper;
     public PatientRecordJobResponse find(Pageable pageable){
         Page<PatientRecordImportJob> pages = patientRecordJobRepository.findAll(pageable);
-        List<PatientRecordImportJob> entities = pages.getContent();
+        List<PatientRecordImportJobDTO> models = pages.getContent()
+                .stream()
+                .map(patientRecordImportJob -> mapper.map(patientRecordImportJob, PatientRecordImportJobDTO.class))
+                .collect(Collectors.toList());
         long total = (pages).getTotalElements();
-        return PatientRecordBuilder.build(total,entities);
+        return PatientRecordBuilder.build(total,models);
     }
 }
