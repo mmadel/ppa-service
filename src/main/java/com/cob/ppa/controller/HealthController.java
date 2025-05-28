7178e71990b5d8c3713fa3edb11b5a4e23ff38ce
madel <mohammed.adel.abo.foutouh@gmail.com>
2025-05-28 13:20:35 +0300

    impl api to expose users

--- src/main/java/com/cob/ppa/controller/HealthController.java
+++ src/main/java/com/cob/ppa/controller/HealthController.java
@@ -3,21 +3,19 @@ package com.cob.ppa.controller;
 import com.cob.ppa.service.user.FindUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/user")
-public class UserController {
+@RequestMapping("/public")
+public class HealthController {
     @Autowired
     FindUserService findUserService;
 
-    @GetMapping("/list")
-    @PreAuthorize("hasRole('ROLE_MODERATOR')")
+    @GetMapping("/ping")
     public ResponseEntity findUsers() {
         return ResponseEntity.ok()
-                .body(findUserService.findAll());
+                .body("live..!!!");
     }
 }
